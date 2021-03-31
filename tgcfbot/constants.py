4fc97d14d966fcd41e0fdb8d1196f93fbde25433
ParsaAlizadeh <parsa.alizadeh1@gmail.com>
2021-03-31 19:20:39 +0430

    Fix line too long

--- tgcfbot/constants.py
+++ tgcfbot/constants.py
@@ -10,6 +10,8 @@ limited_handles = ['tenkei', 'dreamoon_love_AA', 'ReaLNero1'] + \
                   [f'vjudge{i}' for i in range(5)] + \
                   [f'luogu_bot{i}' for i in range(5)]
 
-tags = ['implementation', 'math', 'greedy', 'dp', 'data structures', 'brute force', 'constructive algorithms',
-        'graphs', 'sortings', 'binary search', 'dfs and similar', 'trees', 'strings', 'number theory',
-        'combinatorics', 'two pointers', 'bitmasks', 'geometry', 'dsu']
+tags = [
+    'implementation', 'math', 'greedy', 'dp', 'data structures', 'brute force',
+    'constructive algorithms', 'graphs', 'sortings', 'binary search', 'dfs and similar',
+    'trees', 'strings', 'number theory', 'combinatorics', 'two pointers', 'bitmasks',
+    'geometry', 'dsu']
